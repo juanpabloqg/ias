9b897d765e0dbff3ee86a8a76788a497545a0244
Juan Pablo Quiñones Giraldo <jpqg07@gmail.com>
2018-05-21 07:28:08 -0500

    Se organizan imports, se remueven imports no utilizados

--- src/main/java/com/JP/ias/repository/AvesJpaRepository.java
+++ src/main/java/com/JP/ias/repository/AvesJpaRepository.java
@@ -1,10 +1,8 @@
 package com.JP.ias.repository;
 
 import java.io.Serializable;
-import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.JP.ias.model.TontAvesModel;
